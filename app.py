990cbbb98c695d79c4d1d7d3d95816925601103b
sivaharini kumaravel <153935488+sivaharinikumaravel@users.noreply.github.com>
2025-05-03 11:20:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -94,4 +94,6 @@ def predict():
     return render_template('predict.html', predictions=predictions, year=year)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
